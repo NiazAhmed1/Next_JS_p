7a6f8f642b022a5ec443e91e532f75e3468f7c40
NiazAhmed1 <niazahmedmemon2025@gmail.com>
2025-03-14 01:04:12 +0500

    update app

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="relative bg-black-default">
       <p>
         hello this is Niaz's Portfolio
       </p>
