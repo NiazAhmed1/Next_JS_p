dc31dfb302d503b8cf5bfc79f0ea6868fd4e59a9
NiazAhmed1 <niazahmedmemon2025@gmail.com>
2025-03-16 01:41:59 +0500

    update compoenet

---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+  return (
+
+    <div className='pb-20 pt-36'>
+        <div>
+          <h1 className='text-4xl font-bold text-white text-center'>Welcome to my portfolio</h1>
+          <p className='text-white text-center'>I am a web developer and designer</p>
+
+        </div>      
+    </div>
+  
+)
+}
+
+export default Hero
